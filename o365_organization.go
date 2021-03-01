ed573149ab9fc99277ea224d5a3787d6c0a0b339
Jared Perry <jared.r.perry@gmail.com>
2021-03-01 15:56:35 -0600

    update o365 org object

--- o365_organization.go
+++ o365_organization.go
@@ -11,24 +11,31 @@ type o365Service struct {
 }
 
 type O365Organization struct {
-	ID                    string `json:"uuid"`
-	Name                  string `json:"name"`
-	CompanyID             string `json:"crm"`
-	LocationID            string `json:"location_id"`
-	ContractUUID          string `json:"contract_uuid"`
-	Type                  string `json:"type"`
-	Region                string `json:"region"`
-	FirstBackupTime       int    `json:"first_backup_time"`
-	LastBackupTime        int    `json:"last_backup_time"`
-	TotalUsers            int    `json:"total_users"`
-	TotalBackedUpUsers    int    `json:"total_backedup_users"`
-	TotalLicensedUsers    int    `json:"total_licensed_users"`
-	TotalLicensesConsumed int    `json:"total_licenses_consumed"`
-	UnprotectedUsers      int    `json:"unprotected_users"`
-	BackedUp              bool   `json:"backed_up"`
-	ExchangeOnline        bool   `json:"exchange_online"`
-	Trial                 bool   `json:"trial"`
-	SharepointOnline      bool   `json:"share_point_online"`
+	ID              string `json:"uuid"`
+	Name            string `json:"name"`
+	CompanyID       string `json:"crm"`
+	LocationID      string `json:"location_id"`
+	ContractUUID    string `json:"contract_uuid"`
+	Type            string `json:"type"`
+	Region          string `json:"region"`
+	FirstBackupTime int    `json:"first_backup_time"`
+	LastBackupTime  int    `json:"last_backup_time"`
+
+	TotalUsers               int `json:"total_users"`
+	TotalBackedUpUsers       int `json:"total_backedup_users"`
+	ProtectedActiveUsers     int `json:"protected_active_users"`
+	ProtectedInactiveUsers   int `json:"protected_inactive_users"`
+	TotalProtectedUsers      int `json:"total_protected_users"`
+	ProtectedSharedUsers     int `json:"protected_shared_users"`
+	TotalLicensedUsers       int `json:"total_licensed_users"`
+	UnprotectedUsers         int `json:"unprotected_users"`
+	UnprotectedLicensedUsers int `json:"unprotected_licensed_users"`
+	NumberOfReservedLicenses int `json:"number_of_reserved_licenses"`
+
+	IsTrial            bool `json:"is_trial"`
+	IsExchangeOnline   bool `json:"is_exchange_online"`
+	IsSharePointOnline bool `json:"is_share_point_online"`
+	IsBackedUp         bool `json:"is_backed_up"`
 }
 
 func (s *o365Service) GetOrganization(id string) (O365Organization, error) {
